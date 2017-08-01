53de36550998052cf106a858aa57b991ce89fc18
Yung-Chung Ku <ycku@csie.org>
2017-08-01 12:00:23 +0800

    Create install-ubuntu-amd64.sh

---
+++ docker-hub/install-ubuntu-amd64.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# check out the official page: https://docs.docker.com/engine/installation/linux/docker-ce/ubuntu/
+# This script need to sudo.
+
+# Uninstall old versions
+apt-get remove docker docker-engine docker.io
