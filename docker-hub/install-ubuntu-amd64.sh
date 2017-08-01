9e81b0f1ccd526c373d3c8061196b9e8c9f4753b
Yung-Chung Ku <ycku@csie.org>
2017-08-01 12:57:17 +0800

    Update install-ubuntu-amd64.sh

--- docker-hub/install-ubuntu-amd64.sh
+++ docker-hub/install-ubuntu-amd64.sh
@@ -3,23 +3,23 @@
 # Tested on Ubuntu Zesty 17.04 amd64
 
 # This script need to sudo.
-sudo -v
+sudo -S sudo -v
 
 # Uninstall old versions
-apt-get remove docker docker-engine docker.io
+sudo apt-get remove docker docker-engine docker.io
 
 # Install using the repository
-apt-get update
-apt-get install apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-apt-key fingerprint 0EBFCD88
+sudo apt-get update
+sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
 # pub   4096R/0EBFCD88 2017-02-22
 #       Key fingerprint = 9DC8 5822 9FC7 DD38 854A  E2D8 8D81 803C 0EBF CD88
 # uid                  Docker Release (CE deb) <docker@docker.com>
 # sub   4096R/F273FCD8 2017-02-22
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-apt-get update
-apt-get install docker-ce
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install docker-ce
 
-docker run hello-world
+sudo docker run hello-world
 # You should see the messages of Hello World in the end.
