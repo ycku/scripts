ceb1a3a43c6c577653b1aea857eb3baa1d890b07
Yung-Chung Ku <ycku@csie.org>
2023-04-25 21:51:58 +0800

    Update postgresql-15-rocky-8.sh

--- postgresql/postgresql-15-rocky-8.sh
+++ postgresql/postgresql-15-rocky-8.sh
@@ -1,7 +1,25 @@
 # https://www.postgresql.org/download/linux/redhat/
-sudo dnf install -y https://download.postgresql.org/pub/repos/yum/reporpms/EL-8-x86_64/pgdg-redhat-repo-latest.noarch.rpm
+sudo -v
+xRC=$?
+[[ $xRC -ne 0 ]] && echo "This script need to run sudo" && exit 1
+
 sudo dnf -qy module disable postgresql
-sudo dnf install -y postgresql15-server
-sudo /usr/pgsql-15/bin/postgresql-15-setup initdb
-sudo systemctl enable postgresql-15
-sudo systemctl start postgresql-15
+
+sudo dnf list installed pgdg-redhat-repo
+xRC=$?
+if [[ $xRC -ne 0 ]]; then 
+  sudo dnf install -y https://download.postgresql.org/pub/repos/yum/reporpms/EL-8-x86_64/pgdg-redhat-repo-latest.noarch.rpm
+else
+  sudo dnf update -y https://download.postgresql.org/pub/repos/yum/reporpms/EL-8-x86_64/pgdg-redhat-repo-latest.noarch.rpm
+fi
+
+sudo dnf list installed postgresql15-server
+xRC=$?
+if [[ $xRC -ne 0 ]]; then
+  sudo dnf install -y postgresql15-server
+  sudo /usr/pgsql-15/bin/postgresql-15-setup initdb
+  sudo systemctl enable postgresql-15
+  sudo systemctl start postgresql-15
+else
+  sudo dnf update -y postgresql15-server
+fi
